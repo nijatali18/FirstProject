74ee92a483363c61d10c5667427752bde350e467
Nicat <nijatali@code.edu.az>
2023-05-13 22:50:16 +0400

    Yeni.

---
+++ Layihe2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Layihe2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
